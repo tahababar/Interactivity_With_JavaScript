f50facaaf4b4ab437edc72a2cd98bf3301602826
Taha Babar <tahababar70@gmail.com>
2020-07-02 04:47:20 +0500

    JavaScript

---
+++  Practice/js/practice.js
@@ -0,0 +1,8 @@
+function change(a)
+{
+  if(a == 'Hi'){
+  	document.getElementById("choose").innerHTML = "See, I told you. I changed as you pressed A!";
+  } else{
+    document.getElementById("choosetwo").innerHTML = "See, I told you. I changed as you pressed B!";
+  }
+}
